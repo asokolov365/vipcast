ef242d4a55ab4862ee3cc63a0da62c47361b9fbe
Andrew Sokolov <asokolov@roblox.com>
2024-08-14 22:16:05 -0700

    Break down discovery to discovery and consul

---
+++ discovery/discovery.go
@@ -0,0 +1,184 @@
+// Copyright 2024 Andrew Sokolov
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package discovery implements the vipcast Consul service discovery functionality.
+package discovery
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/VictoriaMetrics/metrics"
+	"github.com/asokolov365/vipcast/config"
+	"github.com/asokolov365/vipcast/lib/consul"
+	"github.com/asokolov365/vipcast/lib/logging"
+	"github.com/asokolov365/vipcast/monitor"
+	"github.com/rs/zerolog"
+	"github.com/valyala/fastrand"
+)
+
+var logger *zerolog.Logger
+
+// Metrics
+var (
+	consulAgentDuration = metrics.NewHistogram(`vipcast_consul_interaction_duration_seconds{action="clients-discovery"}`)
+	// consulHealthCheckDuration = metrics.NewHistogram(`vipcast_consul_interaction_duration_seconds{action="client-healthcheck"}`)
+)
+
+func Init() {
+	if logger == nil {
+		logger = logging.GetSubLogger("discovery")
+	}
+}
+
+// Discovery provides a service discovery client via Consul API
+type Discovery struct {
+	consulConfig *config.ConsulConfig
+}
+
+// NewDiscovery creates a new Discovery
+func NewDiscovery() *Discovery {
+	return &Discovery{
+		consulConfig: config.AppConfig.Consul,
+	}
+}
+
+// findClients is used to query for services on a single node.
+//
+// This finds services that match the provided discovery-tags.
+func (d *Discovery) findClients(ctx context.Context) error {
+	startTime := time.Now()
+	defer consulAgentDuration.UpdateDuration(startTime)
+
+	services, err := consul.ApiClient().NodeServicesByTags(ctx, *d.consulConfig.ClientSDTags)
+	if err != nil {
+		// logger.Error().Err(err).Msgf("unable to get Consul Catalog from %q", *d.consulConfig.HttpAddr)
+		return err
+	}
+	discoveredClients := make(map[string]monitor.Monitor, len(services))
+
+	for _, service := range services {
+		var (
+			vipAddress    string
+			bgpCommString string
+			monitorString string
+		)
+
+		for _, tag := range service.Tags {
+			parts := strings.Split(tag, "=")
+			if len(parts) != 2 {
+				continue
+			}
+			switch parts[0] {
+			case "gocast_vip", "vipcast_vip":
+				vipAddress = parts[1]
+
+			case "gocast_vip_communities", "vipcast_bgp_communities":
+				bgpCommString = parts[1]
+
+			case "gocast_monitor", "vipcast_monitor":
+				monitorString = parts[1]
+			}
+		}
+
+		// Only VipAddress is mandatory
+		if len(vipAddress) > 0 {
+			mon, err := monitor.NewMonitor(service.Service, vipAddress, bgpCommString, monitorString)
+			if err != nil {
+				logger.Warn().Err(err).
+					Str("service", mon.Service()).
+					Msg("unable to create monitor")
+				continue
+			}
+			discoveredClients[vipAddress] = mon
+
+			logger.Debug().
+				Str("service", mon.Service()).
+				Str("vip", mon.VipAddress()).
+				Str("monitor", mon.Type().String()).
+				Msg("found service to vipcast")
+		}
+	}
+
+	logger.Debug().Msgf("spent %d ms in Consul service discovery", time.Since(startTime).Milliseconds())
+
+	// Update Monitor storage with what is discovered in this pass
+	monitor.Storage().Update(discoveredClients)
+
+	return nil
+}
+
+// HealthCheck checks if the service is healthy (passing)
+// func (c *ConsulApiClient) HealthCheck(ctx context.Context, service string) bool {
+// 	startTime := time.Now()
+// 	defer consulHealthCheckDuration.UpdateDuration(startTime)
+
+// 	if useLocalAgent {
+// 		return c.localHealthCheck(ctx, service)
+// 	}
+// 	return c.remoteHealthCheck(ctx, service)
+// }
+
+const jitterMaxMs uint32 = 1000
+
+func (d *Discovery) DiscoverClients(ctx context.Context) {
+	poll := time.Duration(*d.consulConfig.ClientSDInterval) * time.Second
+	ticker := time.NewTicker(poll)
+	defer ticker.Stop()
+
+	logger.Info().Str("interval", fmt.Sprintf("%ds", *d.consulConfig.ClientSDInterval)).
+		Msgf("starting Consul service discovery at %s", *d.consulConfig.HttpAddr)
+
+	// Start immediately, then loop with ticker
+	errCh := make(chan error, 1)
+	go func() { errCh <- d.findClients(ctx) }()
+	select {
+	case <-ctx.Done():
+		<-errCh // Wait for findClients to return.
+		logger.Info().Msgf("stopping Consul service discovery at %s", *d.consulConfig.HttpAddr)
+		return
+	case err := <-errCh:
+		logger.Error().Err(err).Msg("findClients error")
+	default:
+	}
+
+	for {
+		select {
+		case <-ticker.C:
+			errCh := make(chan error, 1)
+			go func() {
+				jitterMs := fastrand.Uint32n(jitterMaxMs)
+				time.Sleep(time.Duration(jitterMs) * time.Millisecond)
+
+				errCh <- d.findClients(ctx)
+			}()
+			select {
+			case <-ctx.Done():
+				<-errCh // Wait for findClients to return.
+				logger.Info().Msgf("stopping Consul service discovery at %s", *d.consulConfig.HttpAddr)
+				return
+			case err := <-errCh:
+				logger.Error().Err(err).Msg("findClients error")
+			default:
+			}
+
+		case <-ctx.Done():
+			logger.Info().Msgf("stopping Consul service discovery at %s", *d.consulConfig.HttpAddr)
+			return
+		}
+	}
+
+}
---
+++ lib/consul/consul.go
@@ -0,0 +1,160 @@
+// Copyright 2024 Andrew Sokolov
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package consul wraps Consul API Client functions
+package consul
+
+import (
+	"context"
+	"strings"
+
+	"github.com/hashicorp/consul/api"
+)
+
+var consulApiClient *apiClient
+
+// ConsulApiClient wraps the Consul service discovery backend
+// and tracks the state of all watched dependencies.
+type apiClient struct {
+	consul        *api.Client
+	agentNodeName string
+	localAgent    bool
+	queryOpts     *api.QueryOptions
+}
+
+// NewApiClient returns a new Consul API client
+func NewApiClient(config *api.Config, node string, queryOpts *api.QueryOptions) error {
+	consulClient, err := api.NewClient(config)
+	if err != nil {
+		return err
+	}
+
+	localAgent := strings.Contains(config.Address, "localhost") ||
+		strings.Contains(config.Address, "127.0.0.1")
+
+	consulApiClient = &apiClient{
+		consul:        consulClient,
+		agentNodeName: node,
+		localAgent:    localAgent,
+		queryOpts:     queryOpts,
+	}
+
+	return nil
+}
+
+func ApiClient() *apiClient {
+	if consulApiClient == nil {
+		panic("BUG: Consul API client is not initialized")
+	}
+	return consulApiClient
+}
+
+// NodeServicesByTags is used to query services on a single Consul Node,
+// filtering the results to include any service that has at least one of the specified tags.
+// In other words, a service will be included if its tags contain either tag1, tag2, or tag3, etc.
+func (c *apiClient) NodeServicesByTags(ctx context.Context, tags []string) ([]*api.AgentService, error) {
+	if err := c.setAgentNodeName(); err != nil {
+		return nil, err
+	}
+
+	svcList, _, err := c.consul.Catalog().NodeServiceList(
+		c.agentNodeName, c.queryOpts.WithContext(ctx),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]*api.AgentService, 0, len(svcList.Services))
+
+	desiredTags := make(map[string]struct{}, len(tags))
+	for _, t := range tags {
+		desiredTags[t] = struct{}{}
+	}
+
+	for _, service := range svcList.Services {
+		for _, tag := range service.Tags {
+			if _, ok := desiredTags[tag]; ok {
+				result = append(result, service)
+				break
+			}
+		}
+	}
+
+	return result, nil
+}
+
+// CatalogServiceByName is used to find a service by name.
+// It wraps api.Catalog.Service()
+func (c *apiClient) CatalogServiceByName(ctx context.Context, service string) ([]*api.CatalogService, error) {
+	services, _, err := c.consul.Catalog().Service(
+		service, "", c.queryOpts.WithContext(ctx),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return services, nil
+}
+
+// ServiceHealthStatus returns the aggregated health status for all services having the specified name.
+// If service is not found, it returns status (critical, nil)
+// If the service is found, it returns status (critical|passing|warning), nil)
+// In all other cases, it returns status (critical, error)
+func (c *apiClient) ServiceHealthStatus(ctx context.Context, service string) (string, error) {
+	if c.localAgent {
+		return c.ServiceHealthStatusOnAgent(ctx, service)
+	}
+	return c.ServiceHealthOnCluster(ctx, service)
+}
+
+// ServiceHealthStatusOnAgent wraps api.Agent.AgentHealthServiceByNameOpts()
+// uses underlying api call: /v1/agent/health/service/name/:service
+func (c *apiClient) ServiceHealthStatusOnAgent(ctx context.Context, service string) (string, error) {
+	health, _, err := c.consul.Agent().AgentHealthServiceByNameOpts(
+		service, c.queryOpts.WithContext(ctx))
+	if err != nil {
+		return api.HealthCritical, err
+	}
+	// maintenance > critical > warning > passing
+	return health, nil
+}
+
+// ServiceHealthOnCluster wraps api.Health.Checks()
+// uses underlying api call: /v1/health/checks/:service
+func (c *apiClient) ServiceHealthOnCluster(ctx context.Context, service string) (string, error) {
+	healthChecks, _, err := c.consul.Health().Checks(
+		service, c.queryOpts.WithContext(ctx))
+	if err != nil {
+		return api.HealthCritical, err
+	}
+	for _, healthCheck := range healthChecks {
+		if healthCheck.Node == c.agentNodeName {
+			return healthCheck.Status, nil
+		}
+	}
+	return api.HealthCritical, nil
+}
+
+// Trying to get NodeName from a Consul Agent
+func (c *apiClient) setAgentNodeName() error {
+	if c.agentNodeName != "" {
+		return nil
+	}
+	name, err := c.consul.Agent().NodeName()
+	if err != nil {
+		return err
+	}
+	c.agentNodeName = name
+	return nil
+}
